d16a94a4cf8a83b1f5d086d4a9614e289e13fb02
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-08-20 10:53:05 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -7,10 +7,10 @@ global.anticall = false
 global.anti_delete = false
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
-global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
+global.ownername ="NurKiy28"
+global.ownernumber = "6289618728196"
+global.botname = "NoerBotZ-NR"
+global.thumbnail = fs.readFileSync("./settings/IMG_20220819_004525.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'e966d3aac613b61dabd35cc6' //register lolhuman.xyz klo mau apikey
 global.zenzkey = '8423e19c13' //register zenzapi.xyz klo mau apikey
